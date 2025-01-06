6fb3bcfa69b3497c4eaf8f9821b2e9698b1ef3c6
LahiruOshara <oshara.16@cse.mrt.ac.lk>
2025-01-06 23:48:09 +0000

    aggregator fix

--- aggregator-service/src/main/java/com/oshara/aggregator_service/AggregatorServiceApplication.java
+++ aggregator-service/src/main/java/com/oshara/aggregator_service/AggregatorServiceApplication.java
@@ -2,6 +2,8 @@ package com.oshara.aggregator_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class AggregatorServiceApplication {
@@ -10,4 +12,11 @@ public class AggregatorServiceApplication {
 		SpringApplication.run(AggregatorServiceApplication.class, args);
 	}
 
+	@RestController
+	static class HealthCheckController {
+		@GetMapping("/")
+		public String healthCheck() {
+			return "OK";
+		}
+	}
 }
