a8b11278bb2f1d33f1af091a45ab95205aff934e
LahiruOshara <oshara.16@cse.mrt.ac.lk>
2025-01-06 23:01:05 +0000

    appointment fix

--- appointment-service/src/main/java/com/oshara/appointment_service/AppointmentServiceApplication.java
+++ appointment-service/src/main/java/com/oshara/appointment_service/AppointmentServiceApplication.java
@@ -1,13 +1,23 @@
 package com.oshara.appointment_service;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import static org.springframework.boot.SpringApplication.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
 public class AppointmentServiceApplication {
 
 	public static void main(String[] args) {
-		run(AppointmentServiceApplication.class, args);
+		SpringApplication.run(AppointmentServiceApplication.class, args);
 	}
 
+	@RestController
+	static class HealthCheckController {
+		@GetMapping("/")
+		public String healthCheck() {
+			return "OK";
+		}
+	}
 }
