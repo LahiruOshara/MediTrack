f9a0665830f5870294384aac201e8ed67bd2197d
LahiruOshara <oshara.16@cse.mrt.ac.lk>
2025-01-06 02:52:28 +0000

    fix app

--- appointment-service/src/main/java/com/oshara/appointment_service/AppointmentServiceApplication.java
+++ appointment-service/src/main/java/com/oshara/appointment_service/AppointmentServiceApplication.java
@@ -1,8 +1,6 @@
 package com.oshara.appointment_service;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import static org.springframework.boot.SpringApplication.*;
 
 @SpringBootApplication
